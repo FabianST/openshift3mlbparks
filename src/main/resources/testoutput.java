318214c359e4e23ed30c4c23c2aaee5cd298ba10
FabianST <fabian.stoeckner@metafinanz.de>
2016-06-07 09:42:56 +0200

    Create testoutput.java

---
+++ src/main/resources/testoutput.java
@@ -0,0 +1,14 @@
+import java.math.* 
+
+public static void main (String args[]){ 
+     int [] outputarray = new int [20]; 
+     double summe = 0; 
+     for (int i=0; i<outputarray.length; i++){ 
+        outputarray[i] = (int) math.random; 
+         summe =+ outputarray[i]; 
+         System.out.println((i+1) +". "outputarray[i]); 
+         if ((i+1) == outputarray.length){ 
+           System.out.println("Summe: "+summe); 
+         } 
+     } 
+ }
